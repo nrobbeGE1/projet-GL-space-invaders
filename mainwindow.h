730a578a5cc77f469ff043ccb26821cf041dbaa0
TheRemi120 <remiajroudi@gmail.com>
2021-06-22 11:13:33 +0200

    Jai creer la premiere ligne de monstres rouges

---
+++ mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+#include "ennemi.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    ennemi *Ennemi[15];
+};
+#endif // MAINWINDOW_H
